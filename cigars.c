aea44038feb7c628fee054ef4cea8201ab4ec352
HasvithaVadlapatla <hasvitha812@gmail.com>
2026-04-22 16:20:53 +0530

    Add files via upload

---
+++ cigars.c
@@ -0,0 +1,37 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int cigars;
+    printf("Enter the number of cigers:\n");
+    scanf("%d",&cigars);
+    
+    int weekend;
+    printf("Enter 1 for weekend and 0 for weekdays:\n");
+    scanf("%d",&weekend);
+    if(weekend==1)
+    {
+        if(cigars>=40)
+        {
+            printf("true");
+        }
+        else
+        {
+            printf("false");
+        }
+    }
+    else
+    {
+        if(cigars>=40 && cigars<=60)
+        {
+            printf("true");
+        }
+        else
+        {
+            printf("false");
+        }
+    }
+    
+
+    return 0;
+}
