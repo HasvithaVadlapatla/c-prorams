e24ae2b820d4b5cdb7248d9b580dd600aae2cc92
HasvithaVadlapatla <hasvitha812@gmail.com>
2026-04-22 15:35:32 +0530

    Add files via upload

---
+++ consonant.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    char ch;
+    printf("Enter the charecter value\n");
+    scanf("%c",&ch);
+    int res=!((ch=='A')||(ch=='E')||(ch=='I')||(ch=='O')||(ch=='U')||(ch=='a')||(ch=='e')||(ch=='i')||(ch=='o')||(ch=='u'));
+    printf("%c is a consonent i.e is :%d",ch,res);
+}
