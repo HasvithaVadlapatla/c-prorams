b21e29c134b21b46145abc66cc7d1a6301e2c4ba
HasvithaVadlapatla <hasvitha812@gmail.com>
2026-04-22 15:37:02 +0530

    Add files via upload

---
+++ greatest.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+
+int main() {
+    int a;
+    int b;
+    printf("Enter the A and B values\n");
+    scanf("%d %d", &a, &b);
+    printf("A value great i.e is: %d", a > b);
+    return 0;
+}
